99d561885257811470da43df266c09bb99f5f485
Andrey Torlopov <i@atorlopov.ru>
2022-10-14 10:35:56 +0300

    Правка ns-объекта

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from app.database import db
 
 from app.views.genre import genre_ns
 from app.views.director import directors_ns
-from app.views.movie import movie_ns
+from app.views.movie import movies_ns
 
 def configure_app(application: Flask):
     ''' Configure flask app. Api with namespaces and create data.'''
@@ -15,7 +15,7 @@ def configure_app(application: Flask):
         api = Api(application)
         api.add_namespace(genre_ns)
         api.add_namespace(directors_ns)
-        api.add_namespace(movie_ns)
+        api.add_namespace(movies_ns)
         create_data(app, db)
 
 
